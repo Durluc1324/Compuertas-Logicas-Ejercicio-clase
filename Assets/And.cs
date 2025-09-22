1ce98b97f953dd025cd31a630de1df4e31fcea3f
Durluc1324 <lucianorodriguezpaven@gmail.com>
2025-09-22 10:19:11 -0300

    Cambios en varias cosas del programa, adición de Diagrama AND OR NOT.svg, ICompuerta.cs, entre otros

---
+++ Assets/And.cs
@@ -0,0 +1,23 @@
+namespace DefaultNamespace;
+
+public class And: ICompuerta
+{
+    public string Nombre { get; set; }
+    public List<int> entradas = new List<int>;
+    public int salida;
+
+    public And()
+    {
+        
+    }
+
+    public void AgregarEntrada(int variable)
+    {
+        this.entradas.Add
+    }
+
+    public int Calcular()
+    {
+        
+    }
+}
